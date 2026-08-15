665bd1908a83e113507de413ad64262603ecbdff
HRitsFadhila <m.harits.fadhila@gmail.com>
2026-08-15 23:00:47 +0800

    feat(user): create view users.create

---
+++ resources/js/Pages/Users/create.vue
@@ -0,0 +1,19 @@
+<script setup>
+import DashboardLayout from '../../Layouts/DashboardLayout.vue';
+
+
+</script>
+
+<template>
+    <DashboardLayout>
+        <div class="mb-6">
+            <h1 class="text-2xl font-bold text-slate-800">Tambah User</h1>
+            <p class="text-slate-500 mt-1">Tambahkan akun user atau admin</p>
+        </div>
+        <div class="bg-white rounded-2xl shadow-sm border-slate-200">
+            <div class="px-6 py-5">
+                <h2 class="font-semibold text-lg">Informasi User</h2>
+            </div>
+        </div>
+    </DashboardLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,26 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
+    if (Auth::check()){
+        return redirect()->route('dashboard');
+    }
     return redirect()->route('login');
 });
 
-Route::get('/login', [LoginController::class, 'view'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
+Route::middleware('guest')->group(function (){
+    Route::get('/login', [LoginController::class, 'view'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+});
+
 
 Route::middleware('auth')->group(function (){
     Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
-    Route::get('/users', [UserController::class,'index'])->name('users');
+
+    Route::get('/users', [UserController::class,'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 });
